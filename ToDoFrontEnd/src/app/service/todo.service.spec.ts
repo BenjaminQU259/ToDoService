e34a85b573abcf797a1730edbd3652831264c7c3
Frank <zongpengchen@gmail.com>
2022-11-07 16:51:44 +0800

    test(czp and minghao): add todo api post service

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ToDoItem } from '../model/ToDoItem';
 
 import { TodoStoreService } from './todo-store.service';
 import { TodoService } from './todo.service';
@@ -7,8 +8,10 @@ describe('TodoService', () => {
 
   let service: TodoService;
   let todoStoreService: TodoStoreService;
+  let httpClient: any;
 
   beforeEach(() => {
+    httpClient = jasmine.createSpyObj('HttpClient', ['post']);
     todoStoreService = new TodoStoreService();
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
@@ -17,4 +20,14 @@ describe('TodoService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create todoItem via mockhttp post', () => {
+    // given
+    const todoItem = new ToDoItem(9, 'title', 'description', true);
+    // when
+    service.create(todoItem);
+    // then
+    expect(httpClient.post).toHaveBeenCalledWith('https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', todoItem)
+
+  })
 });
