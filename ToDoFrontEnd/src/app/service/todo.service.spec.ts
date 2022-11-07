ae2edc7ea594e3db529b7f4557c9d4dc731f4f6f
Minghao QU <MQu2@slb.com>
2022-11-07 17:09:17 +0800

    test(Minghao and Zongpeng): add get all test to todo api service

--- ToDoFrontEnd/src/app/service/todo.service.spec.ts
+++ ToDoFrontEnd/src/app/service/todo.service.spec.ts
@@ -1,20 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+
 import { TodoApiService } from '../api/todo.api.service';
 import { ToDoItem } from '../model/ToDoItem';
-
-import { TodoStoreService } from './todo-store.service';
 import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
 
   let service: TodoService;
-  let todoStoreService: TodoStoreService;
   let httpClient: any;
 
   beforeEach(() => {
     httpClient = jasmine.createSpyObj('HttpClient', ['post']);
-    todoStoreService = new TodoStoreService();
     TestBed.configureTestingModule({
       providers: [
         TodoApiService,
@@ -28,13 +25,19 @@ describe('TodoService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should create todoItem via mockhttp post', () => {
+  it('should create todoItem via mockHttp post', () => {
     // given
     const todoItem = new ToDoItem(9, 'title', 'description', true);
     // when
     service.create(todoItem);
     // then
     expect(httpClient.post).toHaveBeenCalledWith('https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos', todoItem);
-
+  })
+  it('should get todoItems via mockHttp get', () => {
+    // given
+    // when
+    service.getAll();
+    // then
+    expect(httpClient.get).toHaveBeenCalledWith('https://635fc244ca0fe3c21aa3d012.mockapi.io/api/todos');
   })
 });
